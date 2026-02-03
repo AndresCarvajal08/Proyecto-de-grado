62458e99c120ae4ecac29db6bf1b485c7a448b39
Andres <andresters12@gmail.com>
2026-02-02 21:17:51 -0500

    Proyecto inicial de flutter creado

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Ejemplo: Reemplaza estos códigos con los de tus mockups
+  static const Color primario = Color(0xFF123456); // Cambia el 123456 por tu color
+  static const Color secundario = Color(0xFFABCDEF);
+  static const Color acento = Color(0xFFFF5722);
+  static const Color fondo = Color(0xFFF5F5F5);
+}
